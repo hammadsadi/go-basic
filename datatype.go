8ad8b9fff412626671a98d0292e307a604376f16
hammadsadi <hammad.sadi@yahoo.com>
2025-06-01 00:31:45 +0600

    Data Type and Variables Practice

---
+++ datatype.go
@@ -0,0 +1,31 @@
+package main
+import "fmt"
+/*
+int,
+float32,
+bool,
+string,
+*/
+func main(){
+
+	// Variable Declare
+
+	// 1
+	// var x = 20 
+	// var t = true
+	// 2
+	// d :=40
+	// s :=false 
+
+
+	// ========== Variable Declare With Type ===========
+
+	// 1
+	// var x int = 50 
+	// 2
+	// var s bool = false 
+	// 3
+	var a string = 'Sadidseer'
+
+	fmt.Println(a)
+}
